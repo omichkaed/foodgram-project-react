f4acb174cf9bfa2b21603c261e9b73eb9c7dcd85
Валиуллов Илья <omichkaed@yandex.ru>
2023-11-07 03:04:21 +0700

    v2.1 after fix

--- backend/food_back/api/serializers.py
+++ backend/food_back/api/serializers.py
@@ -128,9 +128,8 @@ class FollowSerializer(serializers.ModelSerializer):
 
     def get_is_subscribed(self, obj):
         user = self.context.get('request').user
-        if user.is_anonymous:
-            return False
-        return Follow.objects.filter(user=user, author=obj.id).exists()
+        return Follow.objects.filter(
+            user=user, author=obj.id).exists()
 
     def get_recipes(self, obj):
         request = self.context.get('request')
