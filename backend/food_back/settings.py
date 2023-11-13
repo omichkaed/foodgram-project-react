86b0eca2eea64b9a8fa6464eaee0f7fd1d40a317
omichkaed <omichkaed@yandex.ru>
2023-11-13 19:44:13 +0700

    v4.9 correct back, .env, docker files

--- backend/food_back/settings.py
+++ backend/food_back/settings.py
@@ -64,10 +64,10 @@ WSGI_APPLICATION = 'food_back.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'NAME': os.getenv('POSTGRES_DB', 'foodgram_db'),
+        'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
